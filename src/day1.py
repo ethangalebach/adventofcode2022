77140a0a59c7d2d5f9da2e07a33375288712d9f1
Ethan Galebach <ethan.galebach@gmail.com>
2022-12-04 17:11:18 -0800

    day1 part2

--- src/day1.py
+++ src/day1.py
@@ -1,24 +1,46 @@
 import os
+import bisect
 
-def get_answer(input_path: str) -> float:
-    answer = 0
-    cur_elf = 0
+def get_part1_answer(input_path: str) -> float:
+    max_calories = 0
+    cur_elf_calories = 0
 
     with open(input_path) as f:
         for line in f:
             if line.strip():
-                cur_elf += int(line)
+                cur_elf_calories += int(line)
             else:
-                if cur_elf > answer:
-                    answer = cur_elf
-                cur_elf = 0
+                if cur_elf_calories > max_calories:
+                    max_calories = cur_elf_calories
+                cur_elf_calories = 0
 
-    return answer
+    return max_calories
+
+def get_part2_answer(input_path: str, n_elves: int) -> float:
+    top_n_calories = [0] * n_elves
+    cur_elf_calories = 0
+
+    with open(input_path) as f:
+        for line in f:
+            if line.strip():
+                cur_elf_calories += int(line)
+            else:
+                bisect.insort(top_n_calories, cur_elf_calories)
+                top_n_calories.pop(0)
+                cur_elf_calories = 0
+
+    return sum(top_n_calories)
 
 if __name__ == "__main__":
     filename = os.path.basename(__file__)
     filename = filename.removesuffix('.py') + '.txt'
     file_dir = os.path.dirname(os.path.realpath('__file__'))
     input_path = file_name = os.path.join(file_dir, f'input/{filename}')
-    answer = get_answer(input_path)
-    print(answer)
+
+    part1_answer = get_part1_answer(input_path)
+    part2_answer = get_part2_answer(input_path, 3)
+
+    print(
+        f"Part 1 Answer: {part1_answer},",
+        f"Part 2 Answer: {part2_answer}"
+    )
