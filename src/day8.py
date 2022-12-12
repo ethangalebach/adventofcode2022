9b1019f9c20277f3828156c44bfd36f887807a13
Ethan Galebach <ethan.galebach@gmail.com>
2022-12-11 20:54:02 -0800

    day8 part2

--- src/day8.py
+++ src/day8.py
@@ -25,8 +25,34 @@ class Grid:
             max([self.height_grid[r][col_index] for r in range(row_index+1,self.width)])
         ):
             return True
-
         return False
+
+    def _calc_scenic_score(self, row_index:int, col_index:int) -> int:
+        tree_height = self.height_grid[row_index][col_index]
+        score = 1
+        for east_distance in range(1,col_index+1):
+            if east_distance == col_index or self.height_grid[row_index][col_index] <= self.height_grid[row_index][col_index-east_distance]:
+                score *= east_distance
+                break
+        for west_distance in range(1,self.width-col_index):
+            if west_distance == self.width-col_index-1 or tree_height <= self.height_grid[row_index][col_index+west_distance]:
+                score *= west_distance
+                break
+        for north_distance in range(1,row_index+1):
+            if north_distance == row_index or tree_height <= self.height_grid[row_index-north_distance][col_index]:
+                score *= north_distance
+                break
+        for south_distance in range(1,self.width-row_index):
+            if south_distance == self.width-row_index-1 or tree_height <= self.height_grid[row_index+south_distance][col_index]:
+                score *= south_distance
+                break
+        return score
+
+    def get_max_scenic_score(self) -> int:
+        max_scenic_score = 0
+        for row_index,col_index in product(range(1, self.width-1),repeat=2):
+            max_scenic_score = max(max_scenic_score,self._calc_scenic_score(row_index, col_index))
+        return max_scenic_score
 
 def build_grid(input_path:str) -> Grid:
     height_grid = []
@@ -45,11 +71,11 @@ def build_grid(input_path:str) -> Grid:
     return Grid(height_grid)
 
 def get_answer(input_path: str, part: int) -> str:
+    grid = build_grid(input_path)
     if part == 1:
-        grid = build_grid(input_path)
         return grid.get_count_of_visible_trees()
     elif part == 2:
-        pass
+        return grid.get_max_scenic_score()
     else:
         raise Exception('not part 1 or 2')
 
